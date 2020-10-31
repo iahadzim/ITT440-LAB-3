6127ed0ec5b8e6e3c6087103fd4cffa0c31e00ec
iman amirul <iahadzim@gmail.com>
2020-10-31 17:03:10 +0800

    New 3.1 code updated

--- 3.1.c
+++ 3.1.c
@@ -7,9 +7,21 @@ int main(void)
 {
 
   void sigint_handler(int sig);
+  void sigtstp_handler(int sig);
+  void sigquit_handler(int sig);
   char s[200];
 
   if (signal(SIGINT, sigint_handler) == SIG_ERR){
+     perror("signal");
+     exit(1);
+  }
+  
+  if (signal(SIGTSTP, sigtstp_handler) == SIG_ERR){
+     perror("signal");
+     exit(1);
+  }
+  
+  if (signal(SIGQUIT, sigquit_handler) == SIG_ERR){
      perror("signal");
      exit(1);
   }
@@ -26,6 +38,16 @@ int main(void)
 
 void sigint_handler(int sig)
 {
-  printf("Jangan Kacau saya! \n");
+  printf(" This is a special signal handler for <SIGINT>\n");
+}
+
+void sigtstp_handler(int sig)
+{
+  printf(" This is a special signal handler for <SIGTSTP>\n");
+}
+
+void sigquit_handler(int sig)
+{
+  printf(" This is a special signal handler for <SIGQUIT>\n");
 }
 
